1ea29afa173de005d37daa703e8118f15707b71a
Michael <sie20003@byui.edu>
2023-01-28 16:37:12 +1100

    adding graphql and mongoose dependancies

--- server.js
+++ server.js
@@ -11,45 +11,43 @@ const swaggerDocument = require('./swagger.json');
 const { auth } = require('express-openid-connect');
 
 const config = {
-  authRequired: false,
-  auth0Logout: true,
-  secret: process.env.SECRET,
-  baseURL: process.env.BASEURL,
-  clientID: process.env.CLIENTID,
-  issuerBaseURL: process.env.ISSUER
+	authRequired: false,
+	auth0Logout: true,
+	secret: process.env.SECRET,
+	baseURL: process.env.BASEURL,
+	clientID: process.env.CLIENTID,
+	issuerBaseURL: process.env.ISSUER,
 };
-
 
 // eslint-disable-next-line no-undef
 const port = process.env.PORT || 3000;
 
-app
-  .use(auth(config))
-  .use(express.json())
-  .use(express.urlencoded({ extended: true }))
-  .use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
-  .use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Acccept, Z-Key'
-    );
-    res.setHeader('Content-Type', 'application/json');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONs');
-    next();
-  })
-  .use('/', require('./routes'));
-
+app.use(auth(config))
+	.use(express.json())
+	.use(express.urlencoded({ extended: true }))
+	.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+	.use((req, res, next) => {
+		res.setHeader('Access-Control-Allow-Origin', '*');
+		res.setHeader(
+			'Access-Control-Allow-Headers',
+			'Origin, X-Requested-With, Content-Type, Acccept, Z-Key'
+		);
+		res.setHeader('Content-Type', 'application/json');
+		res.setHeader(
+			'Access-Control-Allow-Methods',
+			'GET, POST, PUT, DELETE, OPTIONs'
+		);
+		next();
+	})
+	.use('/', require('./routes'));
 
 // eslint-disable-next-line no-unused-vars
 mongodb.initDb((err, mongodb) => {
-  if (err) {
-    console.log(err);
-  } else {
-    app.listen(port, () => {
-      console.log(`Connected to DB and listening on port ${port}`)
-    })
-  }
+	if (err) {
+		console.log(err);
+	} else {
+		app.listen(port, () => {
+			console.log(`Connected to DB and listening on port ${port}`);
+		});
+	}
 });
-
-
