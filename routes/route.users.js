2dbede118cbe1d463084c6a627f8d91ce758e327
rodell1983 <ode21001@byui.edu>
2023-02-01 13:40:38 -0800

    test

--- routes/route.users.js
+++ routes/route.users.js
@@ -5,9 +5,8 @@
 const routes = require('express').Router();
 const usersController = require('../controllers/controller.users');
 
-// get all users
-routes.get('/', usersController.getAllUsers);
+
 // checks login state and saves user info
-routes.get('/auth', usersController.isAuthenticated /* #swagger.ignore = true */);
+routes.get('/', usersController.isAuthenticated, usersController.getAllUsers /* #swagger.ignore = true */);
 
 module.exports = routes;
