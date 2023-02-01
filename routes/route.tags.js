99992564ba3396daaca33da25620e07b3c41fcaf
rodell1983 <ode21001@byui.edu>
2023-02-01 13:27:28 -0800

    revert

--- routes/route.tags.js
+++ routes/route.tags.js
@@ -1,2 +1,21 @@
 
+/**
+ * returns various JSON responses from levels collection
+ */
+const routes = require('express').Router();
+const tagsController = require('../controllers/controller.tags');
+const { tagValidate } = require('../validation/tag.validate');
+
+// get all tags
+routes.get('/', tagsController.getAllTags);
+// get tag by id
+routes.get('/:id', tagsController.getTagById);
+// post
+routes.post('/', tagsValidate, tagController.createTag);
+// put
+routes.put('/:id', tagsValidate, tagController.updateTag);
+// delete
+routes.delete('/:id', tagsController.deleteTag);
+
+
 module.exports = routes;
